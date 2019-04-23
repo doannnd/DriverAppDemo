d7512d505c91e493c9a76dab0eebcc60b472afc1
doannd <nguyendinhdoanjdk@gmail.com>
2019-04-23 17:46:58 +0700

    display current location on google

--- app/src/main/java/com/nguyendinhdoan/driverappdemo/MainActivity.java
+++ app/src/main/java/com/nguyendinhdoan/driverappdemo/MainActivity.java
@@ -1,12 +1,12 @@
 package com.nguyendinhdoan.driverappdemo;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -24,6 +24,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.nguyendinhdoan.driverappdemo.model.Driver;
 
+import dmax.dialog.SpotsDialog;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
@@ -210,6 +211,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
+                // disable button sign in if processing
+                signInButton.setEnabled(false);
 
                 final String email = emailEditText.getText().toString();
                 final String password = passwordEditText.getText().toString();
@@ -230,18 +233,24 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
 
+                final AlertDialog waitingDialog = new SpotsDialog.Builder().setContext(MainActivity.this).build();
+                waitingDialog.show();
                 // login user
                 auth.signInWithEmailAndPassword(email, password)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+                            waitingDialog.dismiss();
                             Intent intentWelcome = new Intent(MainActivity.this, WelcomeActivity.class);
                             startActivity(intentWelcome);
                             finish();
                         } else {
+                            waitingDialog.dismiss();
                             Snackbar.make(rootLayout, "Failed: " + task.getException().getMessage(),
                                     Snackbar.LENGTH_LONG).show();
+                            // active sign in button
+                            signInButton.setEnabled(true);
                         }
                     }
                 });
--- app/src/main/java/com/nguyendinhdoan/driverappdemo/WelcomeActivity.java
+++ app/src/main/java/com/nguyendinhdoan/driverappdemo/WelcomeActivity.java
@@ -1,13 +1,241 @@
 package com.nguyendinhdoan.driverappdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.SystemClock;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
-public class WelcomeActivity extends AppCompatActivity {
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
+
+public class WelcomeActivity extends FragmentActivity implements OnMapReadyCallback, CompoundButton.OnCheckedChangeListener {
+
+    private static final String TAG = "ACTIVITY";
+    private GoogleMap mMap;
+
+    private Location mLastLocation;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationCallback locationCallback;
+    private LocationRequest locationRequest;
+
+    private DatabaseReference drivers;
+    private GeoFire geoFire;
+
+    private Marker mCurrent;
+
+    private SupportMapFragment mapFragment;
+    private Switch location_switch;
+    private ConstraintLayout driverLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        setupGoogleMap();
+        initViews();
+        setupDatabase();
+        setupLocation();
+        addEvents();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (location_switch.isChecked()) {
+            startLocationUpdates();
+        }
+    }
+
+    private void setupGoogleMap() {
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.driver_map);
+        mapFragment.getMapAsync(this);
+    }
+
+    private void initViews() {
+        driverLayout = findViewById(R.id.driver_layout);
+        location_switch = findViewById(R.id.location_switch);
+    }
+
+    private void setupDatabase() {
+        drivers = FirebaseDatabase.getInstance().getReference("driversLocation");
+        geoFire = new GeoFire(drivers);
+    }
+
+    private void setupLocation() {
+        fusedLocationProviderClient = new FusedLocationProviderClient(this);
+    }
+
+    private void addEvents() {
+        location_switch.setOnCheckedChangeListener(this);
+    }
+
+    private void stopLocationUpdates() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+    }
+
+    private void displayLocation() {
+        if (mLastLocation != null) {
+            if (location_switch.isChecked()) {
+                geoFire.setLocation(FirebaseAuth.getInstance().getCurrentUser().getUid(),
+                        new GeoLocation(mLastLocation.getLatitude(), mLastLocation.getLongitude()), new GeoFire.CompletionListener() {
+                            @Override
+                            public void onComplete(String key, DatabaseError error) {
+                                if (mCurrent != null) {
+                                    mCurrent.remove();
+                                }
+
+                                mCurrent = mMap.addMarker(new MarkerOptions()
+                                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.car))
+                                        .position(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()))
+                                        .title("You"));
+
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                        new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()),
+                                        15.0f
+                                ));
+
+                                //rotateMarker(mCurrent, -360, mMap);
+                            }
+                        });
+            }
+        }
+    }
+
+    private void rotateMarker(final Marker mCurrent, final float i, GoogleMap mMap) {
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
+        final float startLocation = mCurrent.getRotation();
+        final long duration = 1500;
+
+        final Interpolator interpolator = new LinearInterpolator();
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                long elapsed = SystemClock.uptimeMillis() - start;
+                float t = interpolator.getInterpolation((float) elapsed / duration);
+                float rot = t*i + (1-t)*startLocation;
+                mCurrent.setRotation(-rot > 180 ? rot / 2 : rot);
+                if (t < 1.0) {
+                    handler.postDelayed(this, 16);
+                }
+            }
+        });
+    }
+
+    private void buildLocationRequest() {
+        locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(3000);
+        locationRequest.setSmallestDisplacement(10.0f);
+    }
+
+    private void buildLocationCallback() {
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                mLastLocation = locationResult.getLastLocation();
+                Log.d(TAG, "latitude: " + mLastLocation.getLatitude());
+                Log.d(TAG, "longitude: " + mLastLocation.getLongitude());
+                displayLocation();
+            }
+        };
+    }
+
+    private void startLocationUpdates() {
+        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if (report.areAllPermissionsGranted()) {
+                            buildLocationCallback();
+                            buildLocationRequest();
+
+                            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+                        }
+
+                        if (report.isAnyPermissionPermanentlyDenied()) {
+                            // TODO:
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                })
+                .onSameThread()
+                .check();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) {
+            Snackbar.make(driverLayout, "You are online", Snackbar.LENGTH_LONG).show();
+            startLocationUpdates();
+        } else {
+            Snackbar.make(driverLayout, "You are offline", Snackbar.LENGTH_LONG).show();
+            stopLocationUpdates();
+            mCurrent.remove();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (location_switch.isChecked()) {
+            stopLocationUpdates();
+        }
     }
 }

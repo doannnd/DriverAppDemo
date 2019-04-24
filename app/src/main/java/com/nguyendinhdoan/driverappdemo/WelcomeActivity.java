a166f96887f6512bdd4a20f7d10e78c994bd252e
doannd <nguyendinhdoanjdk@gmail.com>
2019-04-24 09:44:31 +0700

    driver direction and animation for polyline

--- app/src/main/java/com/nguyendinhdoan/driverappdemo/WelcomeActivity.java
+++ app/src/main/java/com/nguyendinhdoan/driverappdemo/WelcomeActivity.java
@@ -1,7 +1,9 @@
 package com.nguyendinhdoan.driverappdemo;
 
 import android.Manifest;
+import android.animation.ValueAnimator;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,10 +16,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.View;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
@@ -27,27 +33,46 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.JointType;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.SquareCap;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.maps.android.PolyUtil;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.nguyendinhdoan.driverappdemo.common.Common;
+import com.nguyendinhdoan.driverappdemo.remote.IGoogleAPI;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class WelcomeActivity extends FragmentActivity implements OnMapReadyCallback, CompoundButton.OnCheckedChangeListener {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class WelcomeActivity extends FragmentActivity implements OnMapReadyCallback, CompoundButton.OnCheckedChangeListener, View.OnClickListener {
 
     private static final String TAG = "ACTIVITY";
     private GoogleMap mMap;
@@ -66,6 +91,81 @@ public class WelcomeActivity extends FragmentActivity implements OnMapReadyCallb
     private Switch location_switch;
     private ConstraintLayout driverLayout;
 
+    // car animation
+    private List<LatLng> polyLineList = new ArrayList<>();
+    private Marker carMarker;
+    private float v;
+    private double lat, lng;
+    private Handler handler;
+    private LatLng startPosition, endPosition, currentPosition;
+    private int index, next;
+    private Button btnGo;
+    private EditText edtPlace;
+    private String destination;
+    private PolylineOptions polylineOptions, blackPolylineOptions;
+    private Polyline blackPolyline, greyPolyline;
+
+    private IGoogleAPI mServices;
+
+    Runnable drawPathRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (index < polyLineList.size() - 1) {
+                index ++;
+                next = index + 1;
+            }
+
+            if (index < polyLineList.size() - 1) {
+                startPosition = polyLineList.get(index);
+                endPosition = polyLineList.get(next);
+            }
+
+            ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, 1);
+            valueAnimator.setDuration(3000);
+            valueAnimator.setInterpolator(new LinearInterpolator());
+            valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    v = animation.getAnimatedFraction();
+                    lng = v*endPosition.longitude + (1-v) * startPosition.longitude;
+                    lat = v*endPosition.latitude + (1-v) * startPosition.latitude;
+                    LatLng newPos = new LatLng(lat, lng);
+                    carMarker.setPosition(newPos);
+                    carMarker.setAnchor(0.5f, 0.5f);
+                    carMarker.setRotation(getBearing(startPosition, newPos));
+                    mMap.moveCamera(CameraUpdateFactory.newCameraPosition(
+                            new CameraPosition.Builder()
+                            .target(newPos)
+                            .zoom(15.0f)
+                            .build()
+                    ));
+                }
+            });
+            valueAnimator.start();
+            handler.postDelayed(this, 3000);
+        }
+    };
+
+    private float getBearing(LatLng startPosition, LatLng endPosition) {
+        double lat = Math.abs(startPosition.latitude - endPosition.latitude);
+        double lng = Math.abs(startPosition.longitude - endPosition.longitude);
+
+        if (startPosition.latitude < endPosition.latitude &&
+                startPosition.longitude < endPosition.longitude) {
+            return (float) (Math.toDegrees(Math.atan(lng / lat)));
+        } else if (startPosition.latitude >= endPosition.latitude &&
+                startPosition.longitude < endPosition.longitude) {
+            return (float) ( (90 - Math.toDegrees(Math.atan(lng / lat))) + 90);
+        } else if (startPosition.latitude >= endPosition.latitude &&
+                startPosition.longitude >= endPosition.longitude) {
+            return (float) (Math.toDegrees(Math.atan(lng / lat)) + 180);
+        } else if (startPosition.latitude < endPosition.latitude &&
+                startPosition.longitude >= endPosition.longitude) {
+            return (float) ((90 - Math.toDegrees(Math.atan(lng / lat))) + 270);
+        }
+        return -1;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -75,15 +175,12 @@ public class WelcomeActivity extends FragmentActivity implements OnMapReadyCallb
         initViews();
         setupDatabase();
         setupLocation();
+        initRetrofit();
         addEvents();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (location_switch.isChecked()) {
-            startLocationUpdates();
-        }
+    private void initRetrofit() {
+        mServices = Common.getGoogleAPI();
     }
 
     private void setupGoogleMap() {
@@ -96,6 +193,8 @@ public class WelcomeActivity extends FragmentActivity implements OnMapReadyCallb
     private void initViews() {
         driverLayout = findViewById(R.id.driver_layout);
         location_switch = findViewById(R.id.location_switch);
+        btnGo = findViewById(R.id.btnGo);
+        edtPlace = findViewById(R.id.edt_place);
     }
 
     private void setupDatabase() {
@@ -109,6 +208,7 @@ public class WelcomeActivity extends FragmentActivity implements OnMapReadyCallb
 
     private void addEvents() {
         location_switch.setOnCheckedChangeListener(this);
+        btnGo.setOnClickListener(this);
     }
 
     private void stopLocationUpdates() {
@@ -129,14 +229,13 @@ public class WelcomeActivity extends FragmentActivity implements OnMapReadyCallb
                                 mCurrent = mMap.addMarker(new MarkerOptions()
                                         .icon(BitmapDescriptorFactory.fromResource(R.drawable.car))
                                         .position(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()))
-                                        .title("You"));
+                                        .title("Your Location"));
 
                                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
                                         new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()),
                                         15.0f
                                 ));
 
-                                //rotateMarker(mCurrent, -360, mMap);
                             }
                         });
             }
@@ -156,7 +255,7 @@ public class WelcomeActivity extends FragmentActivity implements OnMapReadyCallb
             public void run() {
                 long elapsed = SystemClock.uptimeMillis() - start;
                 float t = interpolator.getInterpolation((float) elapsed / duration);
-                float rot = t*i + (1-t)*startLocation;
+                float rot = t * i + (1 - t) * startLocation;
                 mCurrent.setRotation(-rot > 180 ? rot / 2 : rot);
                 if (t < 1.0) {
                     handler.postDelayed(this, 16);
@@ -197,6 +296,10 @@ public class WelcomeActivity extends FragmentActivity implements OnMapReadyCallb
                             buildLocationCallback();
                             buildLocationRequest();
 
+                            if (ActivityCompat.checkSelfPermission(WelcomeActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                                    && ActivityCompat.checkSelfPermission(WelcomeActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                                return;
+                            }
                             fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
                         }
 
@@ -217,6 +320,12 @@ public class WelcomeActivity extends FragmentActivity implements OnMapReadyCallb
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.setTrafficEnabled(false);
+        mMap.setIndoorEnabled(false);
+        mMap.setBuildingsEnabled(false);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
     }
 
     @Override
@@ -227,15 +336,133 @@ public class WelcomeActivity extends FragmentActivity implements OnMapReadyCallb
         } else {
             Snackbar.make(driverLayout, "You are offline", Snackbar.LENGTH_LONG).show();
             stopLocationUpdates();
+            mMap.clear();
+            handler.removeCallbacks(drawPathRunnable);
             mCurrent.remove();
         }
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        if (location_switch.isChecked()) {
-            stopLocationUpdates();
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnGo) {
+            destination = edtPlace.getText().toString();
+            destination = destination.replace(" ", "+"); // replace space with + for fetch data
+            Log.d(TAG, "destination: " + destination);
+
+            getDirection();
         }
+    }
+
+    private void getDirection() {
+        currentPosition = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+
+        String requestApi = null;
+        try {
+            requestApi = "https://maps.googleapis.com/maps/api/directions/json?" +
+                    "mode=drivings&" +
+                    "transit_routing_preference=less_driving&"+
+                    "origin=" + currentPosition.latitude+","+currentPosition.longitude+"&"+
+                    "destination=" + destination + "&" +
+                    "key=" + getString(R.string.google_api_key);
+
+            Log.d(TAG, "URL direction: " + requestApi);
+
+            mServices.getPath(requestApi)
+                    .enqueue(new Callback<String>() {
+                        @Override
+                        public void onResponse(Call<String> call, Response<String> response) {
+                            try {
+                                JSONObject jsonObject = new JSONObject(response.body());
+                                JSONArray jsonArray = jsonObject.getJSONArray("routes");
+                                for (int i = 0; i < jsonArray.length(); i++) {
+                                    JSONObject route = jsonArray.getJSONObject(i);
+                                    JSONObject poly = route.getJSONObject("overview_polyline");
+                                    String polyline = poly.getString("points");
+                                    polyLineList = PolyUtil.decode(polyline);
+                                    Log.d(TAG, "polyline list size" + polyLineList.size());
+                                }
+
+                                // adjusting bound
+                                LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                                for (LatLng latLng : polyLineList) {
+                                    builder.include(latLng);
+                                }
+
+                                LatLngBounds bounds = builder.build();
+                                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, 150);
+                                mMap.moveCamera(cameraUpdate);
+
+                                polylineOptions = new PolylineOptions();
+                                polylineOptions.color(Color.GRAY);
+                                polylineOptions.width(5);
+                                polylineOptions.startCap(new SquareCap());
+                                polylineOptions.endCap(new SquareCap());
+                                polylineOptions.jointType(JointType.ROUND);
+                                polylineOptions.addAll(polyLineList);
+                                greyPolyline = mMap.addPolyline(polylineOptions);
+
+                                blackPolylineOptions = new PolylineOptions();
+                                blackPolylineOptions.color(Color.BLACK);
+                                blackPolylineOptions.width(5);
+                                blackPolylineOptions.startCap(new SquareCap());
+                                blackPolylineOptions.endCap(new SquareCap());
+                                blackPolylineOptions.jointType(JointType.ROUND);
+                                blackPolylineOptions.addAll(polyLineList);
+                                blackPolyline = mMap.addPolyline(blackPolylineOptions);
+
+                                // marker
+                                mMap.addMarker(new MarkerOptions()
+                                        .position(polyLineList.get(polyLineList.size() - 1))
+                                        .title("Pickup Location")
+                                );
+
+                                // animation
+                                ValueAnimator polyLineAnimator = ValueAnimator.ofInt(0, 100);
+                                polyLineAnimator.setDuration(2000);
+                                polyLineAnimator.setInterpolator(new LinearInterpolator());
+                                polyLineAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                                    @Override
+                                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                                        List<LatLng> points = greyPolyline.getPoints();
+                                        int percentValue = (int) valueAnimator.getAnimatedValue();
+                                        Log.d(TAG, "percent value: " + percentValue);
+                                        int size = points.size();
+                                        Log.d(TAG, "size: " + size);
+                                        int newPoints = (int) (size * (percentValue / 100.0f));
+                                        Log.d(TAG, "new Points" + newPoints);
+                                        List<LatLng> p = points.subList(0, newPoints);
+                                        Log.d(TAG, "p" + p.size());
+                                        blackPolyline.setPoints(p);
+                                    }
+                                });
+                                polyLineAnimator.start();
+
+                                carMarker = mMap.addMarker(
+                                        new MarkerOptions().position(currentPosition)
+                                        .flat(true)
+                                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.car))
+                                );
+
+                                handler = new Handler();
+                                index = -1;
+                                next = 1;
+                                handler.postDelayed(drawPathRunnable, 3000);
+
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<String> call, Throwable t) {
+                            Toast.makeText(WelcomeActivity.this, ""+t.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
